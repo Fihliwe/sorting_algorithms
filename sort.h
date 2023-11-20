e8470ac1263670cf4362346816262c999f9d589b
Fihliwe <siyabulelmncube@gmail.com>
2023-11-20 00:12:52 -0800

    fixed swap function in shell sort

--- sort.h
+++ sort.h
@@ -34,6 +34,6 @@ listint_t *s_node(listint_t *node, listint_t **list);
 void swap(int *array, ssize_t item1, ssize_t item2);
 int lomuto_partition(int *array, ssize_t first, ssize_t last, size_t size);
 void quick_sort_implementation(int *array, ssize_t first, ssize_t last, int size);
-void swap(int *array, int item1, int item2);
+void swap_shell(int *array, int item1, int item2);
 
 #endif /* SORT_H */
